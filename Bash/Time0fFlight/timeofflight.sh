ef45d82607c2b022f310e08ba785e096a179203f
Frey0xD <Frey0xD@gmail.com>
2022-02-14 23:42:27 +0530

    ticket

---
+++ Bash/Time0fFlight/timeofflight.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "###########################Thank you for choosing this service ##################################"
+
+printf "where you have to go ? :- \n"
+read flight
+date >> ticket.txt
+printf "\n\n $flight" >> ticket.txt
+
+
+
+
+
+
+
