bbecbb97dcff598c3934215b4cd1b27cfa0da27a
Frey <Frey0xd@gmail.com>
2022-03-17 23:48:55 +0530

    if statement

---
+++ C/YT/if_statement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+  {
+
+
+    int x = 2;
+
+    if (x == 2)
+    {
+      printf("yes it is 2\n");
+    }
+
+    printf("this will run otherwise if it is true ot false\n");
+
+    return 0;
+
+  }
