7908cdc0544ea65e6e3e702e1e6158e1c5d9dadc
frey <frey0xd@gmail.com>
2022-04-25 03:45:09 +0530

    Basic concept

---
+++ Python/1.BC/BC.py
@@ -0,0 +1,76 @@
+print("Hello And Welcome To BC")
+print("Coding is Art")
+life = 42
+print('Hacking is Real Art')
+print('Meaning of life is = ' + str(life))
+
+############################################
+
+print(3+3)
+print(2-4)
+print(3-6+7)
+print(12*2)
+print(50/2) #float 
+print(50//2) # just int 
+print(50%2)
+print(2 * (3 + 4))
+print((4 + 2) / 2 )  
+print(-2 * 2)
+
+# this will give runtimeerrorprint(3826827687436832468**999999999999999)
+
+############################################
+
+#string
+print("hello Frey or hello to you!")
+#Integers
+print(111)
+print(4+5)
+#Floats
+print(6/2)
+print(5/2)
+print(8/3)
+print(44.2)
+print(3.23+5)
+print(2.54*2)
+
+#############################################
+
+print(2**5)
+
+print(2**3**2)
+"""
+     2
+    *
+    *
+  3
+ *
+ *
+2
+
+"""
+print(9 ** (1/2))
+print(9 ** (1//2))
+print(9 ** (1%2))
+
+#END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
