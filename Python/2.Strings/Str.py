bbdf67fbe7c61024e51cb5c937420decb4db8f4c
frey <frey0xd@gmail.com>
2022-04-25 15:14:45 +0530

    The Stings in Python

---
+++ Python/2.Strings/Str.py
@@ -0,0 +1,18 @@
+##########################################################
+
+print("strings!!")
+print("\thello")
+Universe = 42
+print("the meaning of Universe is " + str(Universe))
+a = 45
+b = 77
+c = 11
+d = 61
+trash = "Not in Use Library"
+linux = 17.44
+User = "Frey"
+IP =127.0
+#IP = 127.0.0.1
+# MAC ADDRESS = 3D:55:RR:9012:2022:25
+print(trash + str(linux) + User + str(IP))
+
