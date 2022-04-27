b43e90219828c613fc8c40b410223b977645dbe5
frey <frey0xd@gmail.com>
2022-04-27 23:51:33 +0530

    variables

--- Python/3.Variables/Var.py
+++ Python/3.Variables/Var.py
@@ -1,1 +1,52 @@
-#Var
+#Variables
+""""
+user = "Frey"
+age = 20
+country = "India"
+Fav_Anime = "None"
+
+
+print(user)
+print(Fav_Anime)
+print(user * 3+"\t")
+#del user
+print("welcome to the club :- " + user)
+del user
+user = "Gutts"
+print(user)
+"""
+
+x = "7"
+y = "5"
+z = int(x) + int(y)
+print(z)
+
+
+user_name = input("What is your name = ")
+age = int(input("what is your age ? :-"))
+
+print("ohh i see your name is :- " + user_name)
+print("you are " + str(age) + " years new !") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
