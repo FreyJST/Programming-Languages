f12dc43b0dc21b7fa1aa5648ff1445d464a37f11
frey <frey0xd@gmail.com>
2022-04-29 23:55:45 +0530

    cf

---
+++ Python/4.ControlFlow/CF.py
@@ -0,0 +1,44 @@
+#Booleans
+
+"""my_shit = True
+print(my_shit)
+print(2==3)
+print("hello" == "hello")
+#comparison
+x = 7
+print(x != 8)
+print(x > 5)
+print(x < 2)
+print(x >= 7)
+print(x <= 7)
+print(7 != 8)
+print('f' > 'r')
+print('frey' != 'kira')
+x = (7>5)
+print(int(x))
+"""
+x = 1
+if x > 2:
+    print("x is greater :)")
+if x == 42:
+    print("it's 42")
+if x >= 7:
+    print("have some fun with p3")
+else:
+    print("damn man go and watch some anime")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/4.ControlFlow/else.py
@@ -0,0 +1,14 @@
+#else statements
+"""
+samir = 42
+if samir >= 3:
+    print("samir got fucked!!")
+else:
+    print("samir is safe")
+"""
+
+score = input("what is your score ? :-")
+if (score >= str(70)):
+    print("you win")
+else:
+    print("Try again motherfucker!! if you are a real man\n Never Give up bruh")
