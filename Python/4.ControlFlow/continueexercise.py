9b7dac52f59194b80849e4ae22ee22380b1aa089
frey <free3y@gmail.com>
2022-05-02 23:19:17 +0530

    break and continue

---
+++ Python/4.ControlFlow/continueexercise.py
@@ -0,0 +1,12 @@
+# Ticket prices :) 
+
+total = 0 
+n = 5
+while n > 0:
+    age = int(input("Enter Your age please :-"))
+    n += -1
+    if age < 3:
+        continue
+    else:
+        total += 100
+        print(total)
---
+++ Python/4.ControlFlow/ifwhile.py
@@ -0,0 +1,48 @@
+"""
+
+x = 1
+while x < 10:
+    if x % 2 == 0:
+        print(str(x) + " is even")
+    else:
+        print(str(x) + "is odd")
+        x += 1
+
+x = 0
+while x <= 20:
+    print(x)
+    x +=2 
+
+x = 6
+
+while x > 2:
+    print(x)
+-> this will run in a loop 
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
