7197ce93a36fa346fd1722ea7220f8caf92d56e5
frey <free3y@gmail.com>
2022-05-03 21:34:27 +0530

    List

---
+++ Python/5.Lists/list.py
@@ -0,0 +1,51 @@
+"""cars = ["ferrari" , "bmw" , "tata" , "pagani", "maruti 800"]
+nums = [ 1, 4, 7, 8, 3]
+
+
+print(cars[2])
+print(cars[1])
+print(cars[1] + cars[2])
+print(nums)
+print(str(nums[0]) + cars[2])
+"""
+
+
+# 2-D grids
+# Matrix 
+
+damn =  [
+        [1,2,3],
+        [4,5,6]
+        ]
+
+print(damn[1][2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
