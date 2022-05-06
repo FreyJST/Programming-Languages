764f3b24cf0de01b7b29bac7309367656e2730c7
frey <free3y@gmail.com>
2022-05-06 15:47:08 +0530

    CS club

---
+++ Python/5.Lists/listoperators.py
@@ -0,0 +1,38 @@
+#reassigned 
+names = ["Frey", "Kira", "Luffy", "Zoro", "Samir" ,"johan", "sanji"]
+nums = [ 3,6,7,9,3,2,1 ]
+#nums[0] = 5
+
+#print(nums)
+print(nums + [7,22,77])
+x = [2,3]
+x += [6,8]
+print(x[2]//x[0])
+
+op = [1,2,3]
+print(op * 3)
+
+print("Frey" in names)
+print("Nami" in names)
+if "Frey" in names:
+    print("watch One Piece")
+print(not 6 in nums)
+lol = input("check you names :-")
+if (lol in names):
+    print("you are in CS club")
+else:
+    print("sorry join any other club")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/5.Lists/task.py
@@ -0,0 +1,5 @@
+#Task 
+#Write a Program that takes string and outputs the 3rd character of thestring
+
+a = input("Enter any string :- ")
+print(a[2])
