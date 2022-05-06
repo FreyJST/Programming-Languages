0575f4e727d43a43dbd5fb751884252a4e5284fe
frey <free3y@gmail.com>
2022-05-06 23:27:56 +0530

    for loop in list

---
+++ Python/5.Lists/forlist.py
@@ -0,0 +1,47 @@
+"""
+names = ["luffy", "kira", "johan", "jojo"]
+for gandus in names:
+    print(gandus + "! Damn you are in CS Club")
+
+ 
+
+waifu = "Nammi" #your waifu name
+count = 0
+for x in waifu:
+    if(x == 'm'):
+        count += 1
+
+print(count)
+
+"""
+f = [ 4,7,2,7,2,674,64,12,7,2,7,38,9,2,93,8,2,9]
+count = 0
+for x in f:
+    if(x == 2):
+        count += 1
+        print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
