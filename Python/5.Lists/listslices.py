c98920fe3eee1d0647ffcf5df27a1f5b8d2ed6f5
frey <free3y@gmail.com>
2022-05-09 22:51:46 +0530

    list slices

--- Python/5.Lists/listslices.py
+++ Python/5.Lists/listslices.py
@@ -1,8 +1,14 @@
-square = [2, 45, 774, 43, 64, 2, 7, 9]
+square = [0, 1, 4, 9, 16, 25, 49, 64, 81, 100]
 print(square[3:5])
 print(square[0:8])
 print(square[2:3:5])
-
+print(square[:6])
+print(square[6:])
+print(square[::2])
+print(square[1::2])
+print(square[1:-1])
+print(square[4:0:-4])
+print(square[::-1])
 
 
 
